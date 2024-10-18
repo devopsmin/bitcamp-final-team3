f2a418a11f19da0027082e8f588852b64b8114b8
Baekhyungi <vhxm8155@naver.com>
2024-10-18 18:07:25 +0900

    스프링 부트 적용

--- 05_project/app/src/main/java/project/tripMaker/dao/BoardDao.java
+++ 05_project/app/src/main/java/project/tripMaker/dao/BoardDao.java
@@ -1,11 +1,13 @@
 package project.tripMaker.dao;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import project.tripMaker.vo.Board;
 
 import java.util.List;
 
+@Mapper
 @Repository
 public interface BoardDao {
   List<Board> list() throws Exception;
