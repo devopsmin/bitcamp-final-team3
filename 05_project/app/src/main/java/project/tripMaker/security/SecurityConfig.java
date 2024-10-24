2e3924ae60f68b2fed212f5892e5b3a262574069
Baekhyungi <vhxm8155@naver.com>
2024-10-24 11:07:30 +0900

    User/view.html 수정

--- 05_project/app/src/main/java/project/tripMaker/security/SecurityConfig.java
+++ 05_project/app/src/main/java/project/tripMaker/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
-        .antMatchers("/", "/auth/**", "/css/**", "/js/**", "/images/**", "/schedule/**", "/user/**").permitAll()
+        .antMatchers("/", "/auth/**", "/css/**", "/js/**", "/images/**", "/schedule/**", "/user/**", "/home").permitAll()
         .antMatchers("/admin/**").hasRole("ADMIN")
         .anyRequest().authenticated()
         .and()
